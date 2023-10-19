9e8b03e7915a57634bf4a40b1b37a47253f48c2a
Martin066s <62793025+Martin066s@users.noreply.github.com>
2023-10-19 17:16:13 +0200

    Add files via upload

---
+++ WhatToWear/src/test/java/BigProject/WhatToWear/WhatToWearApplicationTests.java
@@ -0,0 +1,13 @@
+package BigProject.WhatToWear;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WhatToWearApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
